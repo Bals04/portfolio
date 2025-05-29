271a942c3641665891252cf1f3c86090b65c08b6
Bals <balsamojj04@gmail.com>
2025-05-29 12:06:09 +0800

    Added alden animation on button click

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-white text-gray-800">
     <section class="relative bg-gradient-to-br from-white via-gray-100 to-gray-200 py-16 md:py-24">
-      <div class="max-w-7xl mx-auto px-6">
-        <div class="flex flex-col lg:flex-row items-center">
+      <div class="max-w-7xl mx-auto px-6 min-h-screen flex items-center justify-center">
+        <div class="flex flex-col lg:flex-row items-center justify-center w-full">
           <!-- Left column with text and button -->
           <div class="lg:w-3/5 lg:pr-12 z-10">
             <div class="mb-8">
@@ -18,7 +18,7 @@
               businesses the online presence they need to succeed.</p>
             <div class="flex flex-wrap gap-4">
               <div class="mt-16 text-center">
-                <a href="https://facebook.com/jj.balsamo"
+                <!-- <a href="https://facebook.com/jj.balsamo"
                   class="inline-flex items-center px-6 py-3 bg-gradient-to-r from-[#1c1c1c] to-gray-600 text-white font-medium rounded-sm">
                   <span>Let's work together</span>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 ml-2">
@@ -26,7 +26,35 @@
                       d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
                       clip-rule="evenodd" />
                   </svg>
-                </a>
+                </a> -->
+                <button @click="handleClick"
+                  class="inline-flex items-center px-6 py-3 bg-gradient-to-r from-[#1c1c1c] to-gray-600 text-white font-medium rounded-sm">
+                  <span>Let's work together</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 ml-2">
+                    <path fill-rule="evenodd"
+                      d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+                      clip-rule="evenodd" />
+                  </svg>
+                </button>
+
+                <div class="flex items-center mt-16 justify-center lg:justify-start gap-6">
+                  <span class="text-sm text-gray-500 font-medium">Socials:</span>
+                  <a href="https://github.com/bals04" class="text-[#1c1c1c] hover:text-gray-600 transition-colors">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                      fill="currentColor">
+                      <path
+                        d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                    </svg>
+                  </a>
+                  <a href="https://facebook.com/jj.balsamo"
+                    class="text-[#1c1c1c] hover:text-gray-600 transition-colors">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                      fill="currentColor">
+                      <path
+                        d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                    </svg>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
@@ -34,14 +62,13 @@
           <!-- Right column with image and decorative elements -->
           <div class="lg:w-2/5 mt-12 lg:mt-0 relative">
             <div class="relative z-10">
-              <div
-                class="absolute -top-5 -left-5 w-full h-full bg-gradient-to-r from-[#1c1c1c] to-gray-400 opacity-60">
+              <div class="absolute -top-5 -left-5 w-full h-full bg-gradient-to-r from-[#1c1c1c] to-gray-400 opacity-60">
               </div>
               <div
                 class="absolute -bottom-5 -right-5 w-full h-full bg-gradient-to-l from-[#1c1c1c] to-gray-200 opacity-40">
               </div>
               <div class="relative z-20 overflow-hidden shadow-2xl">
-                <img src="/hero2.jpeg" alt="Jonhyl Balsamo" class="w-full h-full object-cover" />
+                <img id="heroImage" src="/hero-1.jpg" alt="Jonhyl Balsamo" class="w-full h-full object-cover" />
               </div>
             </div>
 
@@ -82,21 +109,6 @@
         </div>
 
         <!-- Social links -->
-        <div class="flex items-center mt-16 justify-center lg:justify-start gap-6">
-          <span class="text-sm text-gray-500 font-medium">Socials:</span>
-          <a href="https://github.com/bals04" class="text-[#1c1c1c] hover:text-gray-600 transition-colors">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-              <path
-                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-            </svg>
-          </a>
-          <a href="https://facebook.com/jj.balsamo" class="text-[#1c1c1c] hover:text-gray-600 transition-colors">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-              <path
-                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-            </svg>
-          </a>
-        </div>
       </div>
     </section>
     <section class="py-20 bg-gradient-to-br from-white via-gray-100 to-gray-200 text-gray-800">
@@ -242,6 +254,17 @@ const projects = [
     image: '/enrollmentSystem.png'
   }
 ];
+
+function handleClick() {
+  const img = document.getElementById('heroImage') as HTMLImageElement | null;
+  if (img) {
+    img.src = '/alden_pose.png';
+  } else {
+    console.warn("Image with ID 'heroImage' not found.");
+  }
+}
+
+
 </script>
 
 <style scoped>
