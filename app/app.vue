f20f16d51d24766fb18fa8927c468ffd18c62ad5
Bals <balsamojj04@gmail.com>
2026-07-09 15:18:22 +0800

    added script

--- app/app.vue
+++ app/app.vue
@@ -4,4 +4,15 @@
   </div>
 </template>
 <script setup>
+useHead({
+  script: [
+    {
+      src: 'https://server6611.aworkinsight.dk/browser-ui/chat-widget-loader.js',
+      'data-widget-id': '1e5ed6e4-9ae4-46fd-8051-774eeee1208a',
+      'data-api-base': 'https://server6611.aworkinsight.dk/api',
+      async: true,
+      tagPosition: 'bodyClose'
+    }
+  ]
+})
 </script>
